2b27e665ccb40c60d6c1d71436e0709aac585e58
Tim Ruhkopf <tim.ruhkopf@outlook.de>
2018-12-12 21:30:04 +0100

    tupel classification

--- Skyscraper4x4.py
+++ Skyscraper4x4.py
@@ -35,3 +35,29 @@ zerocase = list(itertools.permutations([1, 2, 3, 4]))
 # now pixel-wise look at the values of the sets with row & columns!
 # depending on the amount of info in those sets, replace those values that are certain and update the row & column sets
 # again look at those that are certain.
+
+
+import itertools
+
+A = list(itertools.permutations([1, 2, 3, 4]))
+
+
+def vis (elem):
+    visable = 1
+    mem = elem[0]
+    for a in elem:
+        if mem < a:
+            mem = a
+            visable += 1
+        else:
+            continue
+    return visable
+
+
+{k: v for k, v in zip(map(vis, A), A)}
+
+clue1 = (2, 2, 1, 3, 2, 2, 3, 1, 1, 2, 2, 3, 3, 2, 1, 3)
+solution1 = ((1, 3, 4, 2), (4, 2, 1, 3), (3, 4, 2, 1), (2, 1, 3, 4))
+
+clue2 = (0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0)
+solution2 = ((2, 1, 4, 3), (3, 4, 1, 2), (4, 2, 3, 1), (1, 3, 2, 4))
