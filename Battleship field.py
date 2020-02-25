27d9f41d87f9be38c14712766a6c8dc75a97d04e
oliverdippel <oliverdippel@gmx.de>
2020-02-25 14:17:32 +0100

    insert battlesquare

--- Battleship field.py
+++ Battleship field.py
@@ -3,6 +3,8 @@ from itertools import permutations
 
 def validate_battlefield(field):
     #https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/python
+    field = np.array(field)
+    battlesquare = [field[i:i + 2, j:j + 2] for i in range(7) for j in range(7)]
 
     per = (set(permutations([1, 1, 1, 0])))
     filter = [np.array(i).reshape(2, 2) for i in per]
