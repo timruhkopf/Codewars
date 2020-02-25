ea85c656247dc12210d60e68311bb14e3f459463
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-02-25 14:46:08 +0100

    Convolution up & running + refactored cyclic filters to oneline

--- Battleship field.py
+++ Battleship field.py
@@ -2,21 +2,22 @@ import numpy as np
 
 
 def validate_battlefield(field):
-    #https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/python
+    # https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/python
     field = np.array(field)
-    battlesquare = [field[i:i + 2, j:j + 2] for i in range(7) for j in range(7)]
 
-    cyclicfilters = [[[1, 1, 1, 0][i - j] for i in range(4)] for j in range(4)] # cyclic permutation
+    # setting up filters for convolution
+    cyclicfilters = [np.array([[1, 1, 1, 0][i - j] for i in range(4)]).reshape(2,2) for j in range(4)]
+    crossfilters = [np.array([[1, 0], [0, 1]]), np.array([[0, 1], [1, 0]])]
 
-    filter = [np.array(f).reshape(2, 2) for f in cyclicfilters]
-
-    for i, j in filter:
-        if np.sum(x * i) == 3:
-            return False
+    # convoltion over field snippets
+    for i in range(7):
+        for j in range(7):
+            for v, filter in zip([3, 2], [cyclicfilters, crossfilters]):
+                if any(map(lambda f: np.sum(f * field[i:i + 2, j:j + 2]) == v, filter)):
+                    return False
 
 
 if __name__ == '__main__':
-
     battlefield = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
                    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
                    [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
@@ -28,4 +29,4 @@ if __name__ == '__main__':
                    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-    validate_battlefield(battlefield) == True
+validate_battlefield(battlefield) == True
