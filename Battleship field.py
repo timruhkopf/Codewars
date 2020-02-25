786e9ae8933422fa1027289ea774ea4d88a0bfce
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-02-25 15:58:34 +0100

    misssing only reset to zero if found

--- Battleship field.py
+++ Battleship field.py
@@ -2,28 +2,39 @@ import numpy as np
 
 
 def validate_battlefield(field):
-    # https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/python
+    """https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/python"""
     field = np.array(field)
 
-    # kill if to many ones
-    if np.sum(field) == 20:
+    # early kill if to many ones
+    ones = np.sum(field)
+    if ones != 20:
         return False
 
     # setting up filters for convolution
-    cyclicfilters = [np.array([[1, 1, 1, 0][i - j] for i in range(4)]).reshape(2,2) for j in range(4)]
+    cyclicfilters = [np.array([[1, 1, 1, 0][i - j] for i in range(4)]).reshape(2, 2) for j in range(4)]
     crossfilters = [np.array([[1, 0], [0, 1]]), np.array([[0, 1], [1, 0]])]
 
-    # convoltion over field snippets
-    for i in range(7):
-        for j in range(7):
+    # convoltion over field snippets (allows early stopping)
+    for i in range(9):
+        for j in range(9):
             for v, filter in zip([3, 2], [cyclicfilters, crossfilters]):
                 if any(map(lambda f: np.sum(f * field[i:i + 2, j:j + 2]) == v, filter)):
                     return False
 
-    ships = [[1]*i for i in reversed(range(2,5))]
+    shipcounter = {k: 0 for k in [4, 3, 2]}
+    for ind, ship in zip([7, 8, 9], [4, 3, 2]):
+        for j in range(10):
+            for i in range(ind):  #
+                if sum(field[j, i:i + ship]) == ship:
+                    shipcounter[ship] += 1
+                if sum(field[i:i + ship, j]) == ship:
+                    shipcounter[ship] += 1
 
-
-    return True
+    shipcounter[1] = np.sum(field)
+    if shipcounter == {4: 1, 3: 2, 2: 3, 1: 4}:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
