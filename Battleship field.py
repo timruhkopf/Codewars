0a1505cd839464a6006627e5cfc87a48e1ec5760
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-02-25 14:15:58 +0100

    added cyclic permutation for edge filters

--- Battleship field.py
+++ Battleship field.py
@@ -1,17 +1,30 @@
 import numpy as np
 from itertools import permutations
 
+
 def validate_battlefield(field):
-    #https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/python
+    # https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/python
 
-    per = (set(permutations([1, 1, 1, 0])))
-    filter = [np.array(i).reshape(2, 2) for i in per]
+    cyclicfilters = [[[1, 1, 1, 0][i - j] for i in range(4)] for j in range(4)] # cyclic permutation
 
-    for i,j in filter:
+    filter = [np.array(f).reshape(2, 2) for f in cyclicfilters]
+
+    for i, j in filter:
         if np.sum(x * i) == 3:
             return False
 
 
+if __name__ == '__main__':
 
+    battlefield = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+                   [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+                   [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+                   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                   [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-
+    validate_battlefield(battlefield) == True
