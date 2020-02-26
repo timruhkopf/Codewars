f8aa42d63313379db146a65f4ded633e163c56ff
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-02-26 18:36:52 +0100

    workaround for mem dictionary access. still needs debugging - particullarly downtown has empty sets in allocation

--- Skyscraper4x4.py
+++ Skyscraper4x4.py
@@ -6,6 +6,19 @@ problemsize = 4
 
 def lazycompute(func):
     """lazily compute the cluekeys"""
+
+    def wrapper(cluekey):
+        if cluekey in mem.keys():
+            # already available
+            return mem[cluekey]
+        elif tuple(reversed(cluekey)) in mem.keys():
+            mem.update({cluekey: list(reversed(mem[tuple(reversed(cluekey))]))})
+        else:
+            mem.update(func(cluekey, basemem))
+
+        # finally return value
+        return mem[cluekey]
+
     # sorting the permutations only once by visability
     permute = list(permutations(list(range(1, problemsize + 1))))
     pclues = {k: [] for k in range(1, problemsize + 1)}
@@ -17,41 +30,21 @@ def lazycompute(func):
         pclues[len(ismax)].append(tup)
 
     # compute base cases (*,0)
-    func.mem = {(k, 0): [set(), set(), set(), set()] for k in range(1, problemsize + 1)}
+    mem = {(k, 0): [set(), set(), set(), set()] for k in range(1, problemsize + 1)}
     for k, lclues in pclues.items():
         for clue in lclues:
             for i, value in enumerate(clue):
-                func.mem[(k, 0)][i].update([value])
-
-    def wrapper(cluekey):
-        if cluekey in func.mem.keys():
-            # already available
-            return func.mem[cluekey]
-        elif tuple(reversed(cluekey)) in func.mem.keys():
-            func.mem[cluekey].update(tuple(reversed(func.mem[tuple(reversed(cluekey))])))
-        else:
-            func.mem.update(func(cluekey))
-
-        # finally return valule
-        return func.mem[cluekey]
-
+                mem[(k, 0)][i].update([value])
+    basemem = mem.copy()
     return wrapper
 
-def recmemo(f):
-    def helper(position, counter):
-        helper.calls += 1
-        helper.memo[position] = f(position, counter)
-        return helper.memo[position]
-
-    helper.calls = 0
-    helper.memo = {}
-    return helper
 
 @lazycompute
-def get_cluevalue(cluekey):
+def get_cluevalue(cluekey,
+                  basedict):  # FIXME do not pass basedict as it is copied in every lookup. instead access memmoized dict immediately!
     """return [cluekey: [set(), set(), set(), set()]} with appropriate sets based on pclues"""
-    return {cluekey: [s0.intersection(s1) for s0, s1 in zip(get_cluevalue.mem[(cluekey[0], 0)],
-                                                            reversed(get_cluevalue.mem[(cluekey[1], 0)]))]}
+    return {cluekey: [s0.intersection(s1) for s0, s1 in zip(basedict[(cluekey[0], 0)],
+                                                            reversed(basedict[(cluekey[1], 0)]))]}
 
 
 def solve_puzzle(clues):
@@ -61,46 +54,20 @@ def solve_puzzle(clues):
     clues = [clue if i % 2 == 0 else list(reversed(clue)) for i, clue in enumerate(clues)]
 
     # columnclues, rowclues = [[(clues[i][k], clues[i + 2][k]) for k in range(4)] for i in [0, 1]]
-    colclues = [(clues[0][k], clues[0 + 2][k]) for k in range(4)]
-    rowclues = [(clues[1 + 2][k], clues[1][k]) for k in range(4)]
+    colclues1 = [(clues[0][k], clues[0 + 2][k]) for k in range(4)]
+    rowclues1 = [(clues[1 + 2][k], clues[1][k]) for k in range(4)]
 
     # (3) looking up clues & computing clue values lazily
-    colclues = list(map(get_cluevalue, colclues))
-    rowclues = list(map(get_cluevalue, rowclues))
+    colclues = list(map(get_cluevalue, colclues1))
+    rowclues = list(map(get_cluevalue, rowclues1))
 
     # (4) bruteforce with recursion & memoize (Sudoku style)
-    matrixindex = list((r, c) for r in range(4) for c in range(4))
+    matrixindex = list((r, c) for r in range(problemsize) for c in range(problemsize))
+    downtown = list(list(0 for i in range(4)) for j in range(problemsize))
 
-    @recmemo
-    def solv(position, counter):
-        '''recursive path trough the problem, whilst considering only applicable paths'''
-        r, c = position
-        S = rowclues[r] & colclues[c]
-        for s in S:
-            if counter + 1 == len(matrixindex):  # base case: last zero value is reached
-                return s
-            else:  # go further down on path with current s
-                rowclues[r].difference_update({s})
-                colclues[c].difference_update({s})
-                sol = solv(matrixindex[counter + 1], counter + 1)
-
-                if bool(sol):  # the next step returned a non empty solution
-                    return s
-                else:  # the next zero index returns {}
-                    # i.e. it has no applicable choices: Go up
-                    # add s to sets it was removed from
-                    rowclues[r].update({s})
-                    colclues[c].update({s})
-                    continue
-        return {}
-
-    solv(position=matrixindex[0], counter=0)
-    # print(solv.memo)
-
-    downtown = tuple(tuple(0 for i in range(4)) for j in range(problemsize))
-
-    for (r, c), val in solv.memo.items():
-        downtown[r][c] = val
+    for (r, c) in matrixindex:
+        # downtown[r][c] = rowclues[r][c] & colclues[c][r]
+        print((r, c), rowclues1[r], colclues1[c], rowclues[r][c], colclues[c][r], rowclues[r][c] & colclues[c][r])
 
 
 if __name__ == '__main__':
