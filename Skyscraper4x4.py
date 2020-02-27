65a7861652fdcbc5b8cbf2a25d4fddaa34f652f4
oliverdippel <oliverdippel@gmx.de>
2020-02-27 16:16:28 +0100

    debug functions. runs up till now

--- Skyscraper4x4.py
+++ Skyscraper4x4.py
@@ -17,6 +17,7 @@ def _sort_permutations():
 
     return pclues
 
+
 def _compute_base_cases():
     pclues = _sort_permutations()
 
@@ -35,6 +36,8 @@ def _compute_base_cases():
 
 
 mem = _compute_base_cases()
+
+
 def lazycompute(func):
     """lazily compute the cluekeys"""
 
@@ -55,14 +58,14 @@ def _get_cluevalue(cluekey):
     """return [cluekey: [set(), set(), set(), set()]} with appropriate sets
     based on cluetuples & the corresponding base cases"""
     return {cluekey: [s0.intersection(s1) for s0, s1 in
-                      zip(mem[(cluekey[0], 0)], mem[(cluekey[1], 0)])]}
+                      zip(mem[(cluekey[0], 0)], mem[(0, cluekey[1])])]}
 
 
 def _interpret_clues(clues):
     # (2) clue parsing: getting row- & columnclues
     lenc = int(len(clues) / 4)  # for adaptive fieled sizes
     clues = [[clues[j * lenc + i] for i in range(lenc)] for j in range(4)]
-    clues = [clue if i % 2 == 0 else list(reversed(clue)) for i, clue in enumerate(clues)]
+    clues = [clue if i in (0,1) else list(reversed(clue)) for i, clue in enumerate(clues)]
 
     colclues = [(clues[0][k], clues[0 + 2][k]) for k in range(4)]
     rowclues = [(clues[1 + 2][k], clues[1][k]) for k in range(4)]
@@ -70,12 +73,12 @@ def _interpret_clues(clues):
     return colclues, rowclues
 
 
-def _preallocate_downtown(colclues, rowclues):
+def _preallocate_downtown(colset, rowset):
     matrixindex = list((r, c) for r in range(problemsize) for c in range(problemsize))
     downtown = list(list(0 for i in range(4)) for j in range(problemsize))
     for (r, c) in matrixindex:
-        # downtown[r][c] = rowclues[r][c] & colclues[c][r]
-        print((r, c), rowclues[r], colclues[c], rowclues[r][c], colclues[c][r], rowclues[r][c] & colclues[c][r])
+        downtown[r][c] = rowset[r][c] & colset[c][r]
+        # print((r, c), rowclues[r], colclues[c], rowclues[r][c], colclues[c][r], rowclues[r][c] & colclues[c][r])
         # print((r, c), get_cluevalue(colclues1[c]), get_cluevalue(rowclues1[r]))
 
     return downtown, matrixindex
@@ -85,11 +88,11 @@ def solve_puzzle(clues):
     colclues, rowclues = _interpret_clues(clues)
 
     # (3) looking up clues & computing clue values lazily
-    colclues = list(map(_get_cluevalue, colclues))
-    rowclues = list(map(_get_cluevalue, rowclues))
+    colset = list(map(_get_cluevalue, colclues))
+    rowset = list(map(_get_cluevalue, rowclues))
 
     # (4) bruteforce with recursion & memoize (Sudoku style)
-    downtown, matrixindex = _preallocate_downtown(colclues, rowclues)
+    downtown, matrixindex = _preallocate_downtown(colset, rowset)
 
 
 if __name__ == '__main__':
@@ -103,25 +106,34 @@ if __name__ == '__main__':
 
         def test_base_case_creation(self):
             mem = _compute_base_cases()
-            self.assertEqual(mem[(2, 0)], {(2, 0): [{1, 2, 3}, {1, 2, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}]})
-            self.assertEqual(mem[(0, 2)], {(0, 2): [{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 4}, {1, 2, 3}]})
-            self.assertEqual(mem[(3, 0)], {(3, 0): [{1, 2}, {1, 2, 3}, {1, 2, 3, 4}, {1, 2, 3, 4}]})
-            self.assertEqual(mem[(0, 4)], {(0, 4): [{4}, {3}, {2}, {1}]})
+            self.assertEqual(mem[(1, 0)], [{4}, {1, 2, 3}, {1, 2, 3}, {1, 2, 3}])
+            self.assertEqual(mem[(0, 1)], [{1, 2, 3}, {1, 2, 3}, {1, 2, 3}, {4}])
+            self.assertEqual(mem[(2, 0)], [{1, 2, 3}, {1, 2, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}])
+            self.assertEqual(mem[(0, 2)], [{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 4}, {1, 2, 3}])
+            self.assertEqual(mem[(3, 0)], [{1, 2}, {1, 2, 3}, {1, 2, 3, 4}, {1, 2, 3, 4}])
+            self.assertEqual(mem[(0, 3)], [{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3}, {1, 2}])
+            self.assertEqual(mem[(4, 0)], [{1}, {2}, {3}, {4}])
+            self.assertEqual(mem[(0, 4)], [{4}, {3}, {2}, {1}])
+
 
         def test_getcluevalue(self):
-            self.assertEqual(_get_cluevalue((2, 0)), {(2, 0): [{1, 2, 3}, {1, 2, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}]},
+            self.assertEqual(_get_cluevalue((2, 0)), [{1, 2, 3}, {1, 2, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}],
                              'Tested, base_case fetch')
-            self.assertEqual(_get_cluevalue(1, 3), {(1, 3): [{4}, {1, 2, 3, 4}, {1, 2, 3}, {1, 2}]},
+            self.assertEqual(_get_cluevalue((1, 3)), [{4}, {1, 2, 3}, {1, 2, 3}, {1, 2}],
                              'Tested creating new values')
-            self.assertEqual(_get_cluevalue((3, 1), _get_cluevalue(1, 3)),
-                             'Tested')
+            self.assertEqual(_get_cluevalue((2, 3)), [{1, 2, 3}, {1, 2, 4}, {1, 2, 3}, {1, 2}],
+                             'Tested creating new values')
+            self.assertEqual(_get_cluevalue((3, 1)), [{1, 2}, {1, 2, 3}, {1, 2, 3}, {4}],
+                             'Tested creating new values')
+            self.assertEqual(_get_cluevalue((3, 1)), list(reversed(_get_cluevalue((1, 3)))),
+                             'Tested lazycompute for reversed')
 
         def test_clueparsing(self):
-            colclues = ((1, 12), (2, 11), (3, 10), (4, 9))
             # FIXME! is the order in rowclues correct? didn't i reverte the rowclues
-            rowclues = ((5, 16), (6, 15), (7, 14), (8, 13))
-            self.assertEqual(_interpret_clues(tuple(i for i in range(1, 17))),
-                             (colclues, rowclues), 'Tested clueparsing')
+            self.assertEqual(_interpret_clues(tuple(i for i in range(1, 17)))[0],
+                             [(1, 12), (2, 11), (3, 10), (4, 9)], 'Tested colclues')
+            self.assertEqual(_interpret_clues(tuple(i for i in range(1, 17)))[1],
+                             [(16, 5), (15, 6), (14, 7), (13, 8)], 'Tested colclues')
 
         def test_preallocate_downtown(self):
             # colclues, rowclues = _interpret_clues(clues)
@@ -178,3 +190,4 @@ if __name__ == '__main__':
 
 
     unittest.main()
+
