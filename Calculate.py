f13a630718dae683434db19e039659cac317c5a5
oliverdippel <oliverdippel@gmx.de>
2020-04-06 11:57:20 +0200

    new challange

---
+++ Calculate.py
@@ -0,0 +1,7 @@
+class Calculator(object):
+  # https://www.codewars.com/kata/5235c913397cbf2508000048
+  def evaluate(self, string):
+    return 0
+
+if __name__ == '__main__':
+    Calculator.evaluate('10 * 5 / 2') == 25
