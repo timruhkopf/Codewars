8b04034b8f82e9cd62a09f83938d4bdeab97fae1
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-04-19 15:35:58 +0200

    parsing & encoding of map added Position classattributes. started on bomb_broadcasting

--- Minesweaper.py
+++ Minesweaper.py
@@ -1,29 +1,27 @@
 class Game:
-    def __init__(self, map, result):
-        """Game class allows interactive debugging in Pycharm
-        :param map: true map"""
+    def __init__(self, map, result=None):
+        """
+        Game class allows interactive debugging in Pycharm
+        :param map: true map
+        """
         self.map = self.parse_map(map)
-        self.result = self.parse_map(result)
-        self.count = 0  # Fixme: number of x in final map
+        if result is not None:
+            self.result = self.parse_map(result)
+            self.count = 0  # Fixme: number of x in result map!
 
     def __repr__(self):
-        return self.map # Fixme: string must be interleaved with spaces & \n
+        return self.encode_map(self.map)
 
-    def parse_map(self, map):
+    @staticmethod
+    def parse_map(map):
         """:return nested list of strings"""
+        return [row.split() for row in map.split('\n')]
 
-        # NOTICE:
-        # """
-        # ...     1 x 1 1 x 1
-        # ...     2 2 2 1 2 2
-        # ...     2 x 2 0 1 x
-        # ...     2 x 2 1 2 2
-        # ...     1 1 1 1 x 1
-        # ...     0 0 0 1 1 1
-        # ...     """ ==
-        #  '\n    1 x 1 1 x 1\n    2 2 2 1 2 2\n    2 x 2 0 1 x\n    2 x 2 1 2 2\n    1 1 1 1 x 1\n    0 0 0 1 1 1\n    '
+    @staticmethod
+    def encode_map(map):
+        """method to get the desired format of the challenge (pass in solved)"""
+        return '\n'.join(' '.join(row) for row in map)
 
-        pass
 
     def open(self, row, column):
         value = self.result[row][column]
@@ -35,34 +33,53 @@ class Game:
         return value
 
 
-
 class Position:
+    clues = dict()  # {position_tuple: Position_instance}
+    rub, cub = 1, 1  # row/colum upper dim of map overwritten @ each solve_mine call!
 
-    def __init__(self):
+    def __init__(self, position, clue):
+        self.position = position
         self.neighbours = None
-        self.clue = None
-        self.bombds = None  # known adjacent bomb positions. len of bombs == clue?
+        self.clue = clue
+        self.adja_bombds = list()  # known adjacent bomb positions. len of bombs == clue?
         # --> uncover all remaining neighbours.
 
-        self.exacly_one = None  # a list of positions, at which it is known to
+        self.exacly_one = None  # a (nested) list of positions, at which it is known to
         # have exacly (one or two) bombs - such that this info may uncover all others
 
-    def broadcast_bomb(self):
-        # (remove the found bomb from self.neighbours)
-        # tell relevant neighbours about the bomb (i.e. the bombs neighbours)
-        pass
+    def broadcast_bomb(self, bomb_position):
+        """(remove the found bomb from self.neighbours)
+        tell relevant neighbours about the bomb (i.e. the bombs neighbours)"""
+
+        bomb_neighb = self._find_neighbours(bomb_position)  # Experimental: when
+        # each field of the board has its own position instance,
+        # this becomes a lookup: Position.clues[bomb_position].neighbours
+
+        for neighb in bomb_neighb:
+            Position.clues[neighb].bomb.extend(bomb_position)
+
+    def _find_neighbours(self, position):
+        """returns the list of all neighbours (excluding self's position).
+        all of them are bound checked"""
+        r, c = position
+        cond = lambda r, c: 0 <= r <= Position.rub and 0 <= c <= Position.cub
+        neighb = [(r + i, c + j)
+                  for i in (-1, 0, 1)
+                  for j in (-1, 0, 1)
+                  if cond(r + i, c + j) and cond(r + i, c + j)]
+        neighb.remove((r, c))
+        return neighb
 
     def die(self):
-        # remove this position instance from classattribute once all bombs
-        # are found & neighbours are uncovered
+        """remove this position instance from classattribute clues once all bombs
+        are found & neighbours are uncovered & informed"""
+        Position.clues.pop(k=self.position)
         pass
 
 
-    game = Game(map)
-    open = game.open
 
 
-def solve_mine(map, n):
+def solve_mine(gamemap, n):
     """
     https://www.codewars.com/kata/57ff9d3b8f7dda23130015fa
 
@@ -72,6 +89,13 @@ def solve_mine(map, n):
     :return: a solved string map, containing only integers & 'x'`s for bomb markers
 
     """
+    # FIXME!
+    game = Game(gamemap)
+    open = game.open
+
+    # to ease Position._find_neighbours() calls
+    Position.rub = len(gamemap[0])
+    Position.cub = len(gamemap)
 
     # uncovering mines using a method "open(row,column)"
     # Game.open(row, column) ????
@@ -82,148 +106,149 @@ def solve_mine(map, n):
     #  but did not modify the map, you should modify the map by yourself.
 
     # call open
-    pos = (1,2)
-    #val = open(*)
+    pos = (1, 2)
+    # val = open(*)
 
     # If the game can not got a valid result, should return "?"
 
     pass
 
 
-if __name__ == '__main__':
+# CAREFULL: if __name__ == '__main__':
+    # indentation & muliline string destroy format!
     # definite & solvable
-    gamemap = """
-    ? ? ? ? ? ?
-    ? ? ? ? ? ?
-    ? ? ? 0 ? ?
-    ? ? ? ? ? ?
-    ? ? ? ? ? ?
-    0 0 0 ? ? ?
-    """.strip()
-    result = """
-    1 x 1 1 x 1
-    2 2 2 1 2 2
-    2 x 2 0 1 x
-    2 x 2 1 2 2
-    1 1 1 1 x 1
-    0 0 0 1 1 1
-    """.strip()
-    game = Game(gamemap, result)
-    assert solve_mine(gamemap, game.count) == result
+gamemap = """
+? ? ? ? ? ?
+? ? ? ? ? ?
+? ? ? 0 ? ?
+? ? ? ? ? ?
+? ? ? ? ? ?
+0 0 0 ? ? ?
+""".strip()
+result = """
+1 x 1 1 x 1
+2 2 2 1 2 2
+2 x 2 0 1 x
+2 x 2 1 2 2
+1 1 1 1 x 1
+0 0 0 1 1 1
+""".strip()
+game = Game(gamemap, result)
+assert solve_mine(gamemap, game.count) == result
 
-    # Ambivalent state
-    gamemap = """
-    0 ? ?
-    0 ? ?
-    """.strip()
-    result = """
-    0 1 x
-    0 1 1
-    """.strip()
-    game = Game(gamemap, result)
-    assert solve_mine(gamemap, game.count) == "?"
+# Ambivalent state
+gamemap = """
+0 ? ?
+0 ? ?
+""".strip()
+result = """
+0 1 x
+0 1 1
+""".strip()
+game = Game(gamemap, result)
+assert solve_mine(gamemap, game.count) == "?"
 
-    gamemap = """
-    ? ? ? ? 0 0 0
-    ? ? ? ? 0 ? ?
-    ? ? ? 0 0 ? ?
-    ? ? ? 0 0 ? ?
-    0 ? ? ? 0 0 0
-    0 ? ? ? 0 0 0
-    0 ? ? ? 0 ? ?
-    0 0 0 0 0 ? ?
-    0 0 0 0 0 ? ?
-    """.strip()
-    result = """
-    1 x x 1 0 0 0
-    2 3 3 1 0 1 1
-    1 x 1 0 0 1 x
-    1 1 1 0 0 1 1
-    0 1 1 1 0 0 0
-    0 1 x 1 0 0 0
-    0 1 1 1 0 1 1
-    0 0 0 0 0 1 x
-    0 0 0 0 0 1 1
-    """.strip()
-    game = Game(gamemap, result)
-    assert solve_mine(gamemap, game.count) == result
+gamemap = """
+? ? ? ? 0 0 0
+? ? ? ? 0 ? ?
+? ? ? 0 0 ? ?
+? ? ? 0 0 ? ?
+0 ? ? ? 0 0 0
+0 ? ? ? 0 0 0
+0 ? ? ? 0 ? ?
+0 0 0 0 0 ? ?
+0 0 0 0 0 ? ?
+""".strip()
+result = """
+1 x x 1 0 0 0
+2 3 3 1 0 1 1
+1 x 1 0 0 1 x
+1 1 1 0 0 1 1
+0 1 1 1 0 0 0
+0 1 x 1 0 0 0
+0 1 1 1 0 1 1
+0 0 0 0 0 1 x
+0 0 0 0 0 1 1
+""".strip()
+game = Game(gamemap, result)
+assert solve_mine(gamemap, game.count) == result
 
-    # Huge ambivalent state
-    gamemap = """
-    ? ? 0 ? ? ? 0 0 ? ? ? 0 0 0 0 ? ? ? 0
-    ? ? 0 ? ? ? 0 0 ? ? ? 0 0 0 0 ? ? ? ?
-    ? ? 0 ? ? ? ? ? ? ? ? 0 0 0 0 ? ? ? ?
-    0 ? ? ? ? ? ? ? ? ? ? 0 0 0 0 0 ? ? ?
-    0 ? ? ? ? ? ? ? ? ? 0 0 0 0 0 0 0 0 0
-    0 ? ? ? 0 0 0 ? ? ? 0 0 0 0 0 0 0 0 0
-    0 0 0 0 0 0 0 ? ? ? ? ? ? ? 0 0 0 0 0
-    0 0 0 0 0 0 0 0 0 0 ? ? ? ? 0 0 0 0 0
-    0 0 ? ? ? 0 ? ? ? 0 ? ? ? ? 0 0 0 0 0
-    0 0 ? ? ? ? ? ? ? 0 0 0 0 0 0 ? ? ? 0
-    0 0 ? ? ? ? ? ? ? ? ? 0 0 0 0 ? ? ? 0
-    0 0 0 0 ? ? ? ? ? ? ? 0 0 0 0 ? ? ? 0
-    0 0 0 0 0 ? ? ? ? ? ? 0 0 0 0 0 ? ? ?
-    0 0 ? ? ? ? ? ? 0 0 0 0 0 0 0 0 ? ? ?
-    0 0 ? ? ? ? ? ? ? 0 0 0 0 0 0 0 ? ? ?
-    0 0 ? ? ? ? ? ? ? ? 0 0 0 0 0 0 0 ? ?
-    0 0 0 0 0 0 ? ? ? ? 0 0 0 ? ? ? 0 ? ?
-    0 0 0 ? ? ? ? ? ? ? 0 0 0 ? ? ? ? ? ?
-    0 0 0 ? ? ? ? ? 0 0 0 ? ? ? ? ? ? ? ?
-    0 0 0 ? ? ? ? ? 0 0 0 ? ? ? 0 ? ? ? ?
-    0 0 0 0 ? ? ? ? ? ? ? ? ? ? 0 ? ? ? ?
-    0 0 0 0 ? ? ? ? ? ? ? ? ? ? 0 ? ? ? ?
-    0 0 0 0 ? ? ? ? ? ? ? ? ? ? 0 ? ? ? ?
-    """.strip()
-    result = """
-    1 1 0 1 1 1 0 0 1 1 1 0 0 0 0 1 1 1 0
-    x 1 0 1 x 1 0 0 2 x 2 0 0 0 0 1 x 2 1
-    1 1 0 2 3 3 1 1 3 x 2 0 0 0 0 1 2 x 1
-    0 1 1 2 x x 1 2 x 3 1 0 0 0 0 0 1 1 1
-    0 1 x 2 2 2 1 3 x 3 0 0 0 0 0 0 0 0 0
-    0 1 1 1 0 0 0 2 x 2 0 0 0 0 0 0 0 0 0
-    0 0 0 0 0 0 0 1 1 1 1 2 2 1 0 0 0 0 0
-    0 0 0 0 0 0 0 0 0 0 1 x x 1 0 0 0 0 0
-    0 0 1 1 1 0 1 1 1 0 1 2 2 1 0 0 0 0 0
-    0 0 1 x 2 1 3 x 2 0 0 0 0 0 0 1 1 1 0
-    0 0 1 1 2 x 3 x 3 1 1 0 0 0 0 1 x 1 0
-    0 0 0 0 1 2 3 2 2 x 1 0 0 0 0 1 1 1 0
-    0 0 0 0 0 1 x 1 1 1 1 0 0 0 0 0 1 1 1
-    0 0 1 1 2 2 2 1 0 0 0 0 0 0 0 0 1 x 1
-    0 0 1 x 2 x 2 1 1 0 0 0 0 0 0 0 1 1 1
-    0 0 1 1 2 1 3 x 3 1 0 0 0 0 0 0 0 1 1
-    0 0 0 0 0 0 2 x x 1 0 0 0 1 1 1 0 1 x
-    0 0 0 1 1 1 1 2 2 1 0 0 0 1 x 1 1 2 2
-    0 0 0 1 x 3 2 1 0 0 0 1 1 2 1 1 1 x 2
-    0 0 0 1 2 x x 1 0 0 0 1 x 1 0 1 2 3 x
-    0 0 0 0 1 2 2 1 1 1 1 1 1 1 0 1 x 3 2
-    0 0 0 0 1 1 1 1 2 x 1 1 1 1 0 2 3 x 2
-    0 0 0 0 1 x 1 1 x 2 1 1 x 1 0 1 x 3 x
-    """.strip()
-    game = Game(gamemap, result)
-    assert solve_mine(gamemap, game.count) == "?"
+# Huge ambivalent state
+gamemap = """
+? ? 0 ? ? ? 0 0 ? ? ? 0 0 0 0 ? ? ? 0
+? ? 0 ? ? ? 0 0 ? ? ? 0 0 0 0 ? ? ? ?
+? ? 0 ? ? ? ? ? ? ? ? 0 0 0 0 ? ? ? ?
+0 ? ? ? ? ? ? ? ? ? ? 0 0 0 0 0 ? ? ?
+0 ? ? ? ? ? ? ? ? ? 0 0 0 0 0 0 0 0 0
+0 ? ? ? 0 0 0 ? ? ? 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 ? ? ? ? ? ? ? 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 ? ? ? ? 0 0 0 0 0
+0 0 ? ? ? 0 ? ? ? 0 ? ? ? ? 0 0 0 0 0
+0 0 ? ? ? ? ? ? ? 0 0 0 0 0 0 ? ? ? 0
+0 0 ? ? ? ? ? ? ? ? ? 0 0 0 0 ? ? ? 0
+0 0 0 0 ? ? ? ? ? ? ? 0 0 0 0 ? ? ? 0
+0 0 0 0 0 ? ? ? ? ? ? 0 0 0 0 0 ? ? ?
+0 0 ? ? ? ? ? ? 0 0 0 0 0 0 0 0 ? ? ?
+0 0 ? ? ? ? ? ? ? 0 0 0 0 0 0 0 ? ? ?
+0 0 ? ? ? ? ? ? ? ? 0 0 0 0 0 0 0 ? ?
+0 0 0 0 0 0 ? ? ? ? 0 0 0 ? ? ? 0 ? ?
+0 0 0 ? ? ? ? ? ? ? 0 0 0 ? ? ? ? ? ?
+0 0 0 ? ? ? ? ? 0 0 0 ? ? ? ? ? ? ? ?
+0 0 0 ? ? ? ? ? 0 0 0 ? ? ? 0 ? ? ? ?
+0 0 0 0 ? ? ? ? ? ? ? ? ? ? 0 ? ? ? ?
+0 0 0 0 ? ? ? ? ? ? ? ? ? ? 0 ? ? ? ?
+0 0 0 0 ? ? ? ? ? ? ? ? ? ? 0 ? ? ? ?
+""".strip()
+result = """
+1 1 0 1 1 1 0 0 1 1 1 0 0 0 0 1 1 1 0
+x 1 0 1 x 1 0 0 2 x 2 0 0 0 0 1 x 2 1
+1 1 0 2 3 3 1 1 3 x 2 0 0 0 0 1 2 x 1
+0 1 1 2 x x 1 2 x 3 1 0 0 0 0 0 1 1 1
+0 1 x 2 2 2 1 3 x 3 0 0 0 0 0 0 0 0 0
+0 1 1 1 0 0 0 2 x 2 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 1 1 1 1 2 2 1 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 1 x x 1 0 0 0 0 0
+0 0 1 1 1 0 1 1 1 0 1 2 2 1 0 0 0 0 0
+0 0 1 x 2 1 3 x 2 0 0 0 0 0 0 1 1 1 0
+0 0 1 1 2 x 3 x 3 1 1 0 0 0 0 1 x 1 0
+0 0 0 0 1 2 3 2 2 x 1 0 0 0 0 1 1 1 0
+0 0 0 0 0 1 x 1 1 1 1 0 0 0 0 0 1 1 1
+0 0 1 1 2 2 2 1 0 0 0 0 0 0 0 0 1 x 1
+0 0 1 x 2 x 2 1 1 0 0 0 0 0 0 0 1 1 1
+0 0 1 1 2 1 3 x 3 1 0 0 0 0 0 0 0 1 1
+0 0 0 0 0 0 2 x x 1 0 0 0 1 1 1 0 1 x
+0 0 0 1 1 1 1 2 2 1 0 0 0 1 x 1 1 2 2
+0 0 0 1 x 3 2 1 0 0 0 1 1 2 1 1 1 x 2
+0 0 0 1 2 x x 1 0 0 0 1 x 1 0 1 2 3 x
+0 0 0 0 1 2 2 1 1 1 1 1 1 1 0 1 x 3 2
+0 0 0 0 1 1 1 1 2 x 1 1 1 1 0 2 3 x 2
+0 0 0 0 1 x 1 1 x 2 1 1 x 1 0 1 x 3 x
+""".strip()
+game = Game(gamemap, result)
+assert solve_mine(gamemap, game.count) == "?"
 
-    # differently shaped ambivalent state
-    gamemap = """
-    0 0 0 0 0 0 0 0 ? ? ? ? ? 0 ? ? ? 0 ? ? ?
-    0 0 0 0 0 0 0 0 ? ? ? ? ? 0 ? ? ? ? ? ? ?
-    0 0 0 0 0 0 0 0 0 0 ? ? ? 0 ? ? ? ? ? ? ?
-    0 0 0 0 0 ? ? ? 0 0 ? ? ? 0 ? ? ? ? ? ? 0
-    ? ? 0 0 0 ? ? ? 0 ? ? ? ? 0 0 ? ? ? ? ? ?
-    ? ? 0 0 0 ? ? ? 0 ? ? ? 0 0 0 ? ? ? ? ? ?
-    ? ? ? 0 0 0 0 0 0 ? ? ? 0 0 0 0 0 0 ? ? ?
-    ? ? ? 0 0 0 0 0 0 0 ? ? ? ? 0 0 ? ? ? 0 0
-    ? ? ? 0 0 0 0 0 0 0 ? ? ? ? 0 0 ? ? ? 0 0
-    """.strip()
-    result = """
-    0 0 0 0 0 0 0 0 1 x x 2 1 0 1 x 1 0 1 2 x
-    0 0 0 0 0 0 0 0 1 2 3 x 1 0 2 2 2 1 2 x 2
-    0 0 0 0 0 0 0 0 0 0 2 2 2 0 1 x 1 1 x 2 1
-    0 0 0 0 0 1 1 1 0 0 1 x 1 0 1 2 2 2 1 1 0
-    1 1 0 0 0 1 x 1 0 1 2 2 1 0 0 1 x 1 1 1 1
-    x 1 0 0 0 1 1 1 0 1 x 1 0 0 0 1 1 1 1 x 1
-    2 2 1 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 1 1 1
-    1 x 1 0 0 0 0 0 0 0 1 2 2 1 0 0 1 1 1 0 0
-    1 1 1 0 0 0 0 0 0 0 1 x x 1 0 0 1 x 1 0 0
-    """.strip()
-    game = Game(gamemap, result)
-    assert solve_mine(gamemap, game.count) == "?"
+# differently shaped ambivalent state
+gamemap = """
+0 0 0 0 0 0 0 0 ? ? ? ? ? 0 ? ? ? 0 ? ? ?
+0 0 0 0 0 0 0 0 ? ? ? ? ? 0 ? ? ? ? ? ? ?
+0 0 0 0 0 0 0 0 0 0 ? ? ? 0 ? ? ? ? ? ? ?
+0 0 0 0 0 ? ? ? 0 0 ? ? ? 0 ? ? ? ? ? ? 0
+? ? 0 0 0 ? ? ? 0 ? ? ? ? 0 0 ? ? ? ? ? ?
+? ? 0 0 0 ? ? ? 0 ? ? ? 0 0 0 ? ? ? ? ? ?
+? ? ? 0 0 0 0 0 0 ? ? ? 0 0 0 0 0 0 ? ? ?
+? ? ? 0 0 0 0 0 0 0 ? ? ? ? 0 0 ? ? ? 0 0
+? ? ? 0 0 0 0 0 0 0 ? ? ? ? 0 0 ? ? ? 0 0
+""".strip()
+result = """
+0 0 0 0 0 0 0 0 1 x x 2 1 0 1 x 1 0 1 2 x
+0 0 0 0 0 0 0 0 1 2 3 x 1 0 2 2 2 1 2 x 2
+0 0 0 0 0 0 0 0 0 0 2 2 2 0 1 x 1 1 x 2 1
+0 0 0 0 0 1 1 1 0 0 1 x 1 0 1 2 2 2 1 1 0
+1 1 0 0 0 1 x 1 0 1 2 2 1 0 0 1 x 1 1 1 1
+x 1 0 0 0 1 1 1 0 1 x 1 0 0 0 1 1 1 1 x 1
+2 2 1 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 1 1 1
+1 x 1 0 0 0 0 0 0 0 1 2 2 1 0 0 1 1 1 0 0
+1 1 1 0 0 0 0 0 0 0 1 x x 1 0 0 1 x 1 0 0
+""".strip()
+game = Game(gamemap, result)
+assert solve_mine(gamemap, game.count) == "?"
