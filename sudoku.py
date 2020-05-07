cc7e77a46eed91ef525ef7baacc3953a199ec58d
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-05-07 09:28:00 +0200

    minor refactoring

--- sudoku.py
+++ sudoku.py
@@ -1,24 +1,3 @@
-problem = [[9, 0, 0, 0, 8, 0, 0, 0, 1],
-           [0, 0, 0, 4, 0, 6, 0, 0, 0],
-           [0, 0, 5, 0, 7, 0, 3, 0, 0],
-           [0, 6, 0, 0, 0, 0, 0, 4, 0],
-           [4, 0, 1, 0, 6, 0, 5, 0, 8],
-           [0, 9, 0, 0, 0, 0, 0, 2, 0],
-           [0, 0, 7, 0, 3, 0, 2, 0, 0],
-           [0, 0, 0, 7, 0, 5, 0, 0, 0],
-           [1, 0, 0, 0, 4, 0, 0, 0, 7]]
-
-solution = [[9, 2, 6, 5, 8, 3, 4, 7, 1],
-            [7, 1, 3, 4, 2, 6, 9, 8, 5],
-            [8, 4, 5, 9, 7, 1, 3, 6, 2],
-            [3, 6, 2, 8, 5, 7, 1, 4, 9],
-            [4, 7, 1, 2, 6, 9, 5, 3, 8],
-            [5, 9, 8, 3, 1, 4, 7, 2, 6],
-            [6, 5, 7, 1, 3, 8, 2, 9, 4],
-            [2, 8, 4, 7, 9, 5, 6, 1, 3],
-            [1, 3, 9, 6, 4, 2, 8, 5, 7]]
-
-
 def memoize(f):
     def helper(position, counter):
         helper.calls += 1
@@ -29,7 +8,8 @@ def memoize(f):
     helper.memo = {}
     return helper
 
-def solve (problem):
+
+def solve(problem):
     '''Solves the sudoku, by reducing the number of paths to the applicable only.'''
 
     # index on (row , column, block) = (i, j, i//3 + j//3 + (i//3)*2).
@@ -46,7 +26,7 @@ def solve (problem):
     zero = [(r, c, b) for r, c, b in sudokuindex if problem[r][c] == 0]
 
     @memoize
-    def solv (position, counter):
+    def solv(position, counter):
         '''recursive path trough the problem, whilst considering only applicable paths'''
         r, c, b = position
         S = candrow[r] & candcol[c] & candblock[b]
@@ -59,7 +39,8 @@ def solve (problem):
                 candblock[b].difference_update({s})
                 sol = solv(zero[counter + 1], counter + 1)
 
-                if bool(sol):  # the next step returned a non empty solution
+                if bool(sol):
+                    # the next step returned a non empty solution
                     return s
                 else:  # the next zero index returns {}
                     # i.e. it has no applicable choices: Go up
@@ -68,7 +49,7 @@ def solve (problem):
                     candcol[c].update({s})
                     candblock[b].update({s})
                     continue
-        return {}
+        return None
 
     solv(position=zero[0], counter=0)
     print('it required {} calls to solve the problem'.format(solv.calls))
@@ -78,5 +59,26 @@ def solve (problem):
         problem[r][c] = val
 
     return problem
+
+if __name__ == '__main__':
+    problem = [[9, 0, 0, 0, 8, 0, 0, 0, 1],
+               [0, 0, 0, 4, 0, 6, 0, 0, 0],
+               [0, 0, 5, 0, 7, 0, 3, 0, 0],
+               [0, 6, 0, 0, 0, 0, 0, 4, 0],
+               [4, 0, 1, 0, 6, 0, 5, 0, 8],
+               [0, 9, 0, 0, 0, 0, 0, 2, 0],
+               [0, 0, 7, 0, 3, 0, 2, 0, 0],
+               [0, 0, 0, 7, 0, 5, 0, 0, 0],
+               [1, 0, 0, 0, 4, 0, 0, 0, 7]]
+
+    solution = [[9, 2, 6, 5, 8, 3, 4, 7, 1],
+                [7, 1, 3, 4, 2, 6, 9, 8, 5],
+                [8, 4, 5, 9, 7, 1, 3, 6, 2],
+                [3, 6, 2, 8, 5, 7, 1, 4, 9],
+                [4, 7, 1, 2, 6, 9, 5, 3, 8],
+                [5, 9, 8, 3, 1, 4, 7, 2, 6],
+                [6, 5, 7, 1, 3, 8, 2, 9, 4],
+                [2, 8, 4, 7, 9, 5, 6, 1, 3],
+                [1, 3, 9, 6, 4, 2, 8, 5, 7]]
 
 print(solve(problem) == solution)
