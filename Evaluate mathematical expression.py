83a619b63cb531b41222b8509106b0c20dbb4a7d
oliverdippel <oliverdippel@gmx.de>
2020-05-16 13:26:45 +0200

    pairs of sorted brackets

--- Evaluate mathematical expression.py
+++ Evaluate mathematical expression.py
@@ -1,10 +1,24 @@
 def calc(expression):
-    pass
+    left = [i for i, _ in enumerate(expression) if _ == '(']
+    right = [i for i, _ in enumerate(expression) if _ == ')']
+
+    # find pairs of sorted brackets
+    pairs = []
+    for i in left[::-1]:
+        for j in right:
+            if j > i:
+                pairs.append((i, j))
+                left.remove(i)
+                right.remove(j)
+
+
+
+
 
 
 if __name__ == '__main__':
-    calc('-7 * -(6 / 3)') == 14
-    calc('(2 / (2 + 3.33) * 4) - -6') == 7.50093808630394
+    # calc('-7 * -(6 / 3)') == 14
+    calc('(2 / (2 + 3.33) * 4) - -6*(2+(3-1))') == 7.50093808630394
 
 
 
