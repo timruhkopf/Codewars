211d47cc706fa6c311bea019fb33853a0cbd0042
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-05-24 15:00:24 +0200

    flat express in function

--- Evaluate mathematical expression.py
+++ Evaluate mathematical expression.py
@@ -1,4 +1,9 @@
 import re
+from operator import mul, add, truediv
+from collections import deque, defaultdict
+
+subdeq = deque()
+op = defaultdict(lambda: add, {'/': truediv, '*': mul})
 
 
 def calc(expression):
@@ -14,44 +19,67 @@ def calc(expression):
                 pairs.append((i, j))
                 left.remove(i)
                 right.remove(j)
+                break
+
+    # # validate braket pairs:
+    # for tup in pairs:
+    #     print(expression[tup[0] + 1:tup[1]])
 
     # figure out nesting structure of parenthesis
     nested = lambda y, x: x[0] > y[0] and x[1] < y[1]  # x nested in y
-    brackets = {k: [v for v in pairs if nested(k, v)] for k in pairs}
+    brackets = {k: set(v for v in pairs if nested(k, v)) for k in pairs}
+    bracket_order = {k: len(brackets[k]) for k in pairs}
+
+    # # filter brackets such, that only the directly next nested level is avail.
+    # for k in [k for k in brackets.keys() if bracket_order[k]!= 0]:
+    #     brackets[k].difference_update(set.union(*(brackets[key] for key in brackets[k] if bracket_order[key]!= 0)))
+    #
+    # for k in brackets.keys():
+    #     for tupy in brackets[k]:
+    #         for tupx in brackets[k]:
+    #             if nested(tupy,tupx):
+    #                 brackets[k].remove(tupx)
+
+    # start with 0 len and evaluate.
+
+    # continue with level one and replace
 
     # execution order by length of brackets !
-    regex = re.compile(r'([\/\*])?([\-]?\d*\.\d+|[\-]?\d+)') # almost correct
+    regex = re.compile(r'([\/\*])?([\-]?\d*\.\d+|[\-]?\d+)')  # almost correct
 
     # re.split('\+|\-|\*|\/', '1.2+2-3/6*4')  # separating numbers by operator delimiter
     # re.findall('[+\-\*\/]+', '1.2+2-3/-6*4')  # find all operations (including '/-')
 
-
     for sl in sorted(brackets, key=lambda k: len(brackets[k])):
         if len(brackets[sl]) == 0:
-            regex.findall(expression[sl[0]+1:sl[1]])
+            str(eval_flat_expression(regex.findall(expression[sl[0] + 1:sl[1]])))
 
-    # TODO carefull evaluate if no brackets exist!
-    # evaluate the entire expression with all repalaced brackets!
-    regex.findall(expression)
-
-
-        # at execution pull the slice and replace the nested brackets by their literal value
-        # carefull to replace starting from behind to not mess up the indicies
-
-        # regex over flattened sub expression
-
-        # search for devision & multiplication
-
-        # do addition
 
 
 
     print()
 
 
+def eval_flat_expression(expression):
+    global subdeq
+    global op
+
+    for operator, val in expression:
+        if operator not in ['*', '/']:
+            subdeq.append((operator, float(val)))
+        else:
+            prev = subdeq.pop()
+            subdeq.append(('', op[operator](prev[1], float(val))))
+
+    bracket_content = sum(val for _, val in subdeq)
+    subdeq.clear()
+    return bracket_content
+
+
 if __name__ == '__main__':
-    calc('13 * 36 / 5 / -76 * 71 - 70 * -21 - -80')
+    calc('(-64) * (94 / -13 / -(3)) - (62 * -((((-45 + 46)))) + -6)')  # carefull *-
     calc('(2+2) + (3-3) * (2 / (2 + 3.33) * 4) - -6 * (3-(3/1))')
+    calc('13 * 36 / 5 / -76 * 71 - 70 * -21 - -80')
     calc('-7 * -(6 / 3)') == 14
     calc('(2 / (2 + 3.33) * 4) - -6') == 7.50093808630394
     calc('(2+2) + (3-3)')
@@ -63,6 +91,4 @@ if __name__ == '__main__':
     calc('(((((-1)))))')
 
 
-    calc('(-64) * (94 / -13 / -(3)) - (62 * -((((-45 + 46)))) + -6)')  # carefull *-
     calc('-(22) * (-14 / 32 + (52)) - (-12 + (((-(23 / 74)))) + 50)')
-
