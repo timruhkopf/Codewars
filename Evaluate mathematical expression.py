5b8cdcc4795c79cc465dfce3a0ccb44d544f3c46
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-05-25 11:29:46 +0200

    succeeded in combat

--- Evaluate mathematical expression.py
+++ Evaluate mathematical expression.py
@@ -24,7 +24,7 @@ def calc(expression):
     # figure out (entire) nesting structure of parenthesis
     nested = lambda y, x: x[0] > y[0] and x[1] < y[1]  # x nested in y
     brackets = {k: list(v for v in pairs if nested(k, v)) for k in pairs}
-    bracket_order = {k: len(brackets[k]) for k in pairs} # hierarchy level
+    bracket_order = {k: len(brackets[k]) for k in pairs}  # hierarchy level
 
     # keep only first inner brackets
     for k in brackets.keys():
@@ -33,36 +33,30 @@ def calc(expression):
                 if nested(tup, nest):
                     brackets[k].remove(nest)
 
-    for br in brackets.keys():
-        print(expression[br[0]+1:br[1]], '==', eval(expression[br[0]+1:br[1]]))
-
     # replace the lowest hierarchy brackets
-    regex = re.compile(r'([\/\*])?([\-]?\d*\.\d+|[\-]?\d+)')
+    regex = re.compile(r'([\/\*])?([\+\-]?\d*\.\d+|[\+\-]?\d+)')
+    regex.findall('-35*-5')
     for sl in [k for k, v in bracket_order.items() if v == 0]:
         brackets[sl] = str(eval_flat_expression(regex.findall(expression[sl[0] + 1:sl[1]])))
 
     # replace each bracket hierarchy's lower levels
     # reverse ensures, that replacements do not change the indicies!
-    for i in sorted(set(bracket_order.values())- {0}):
+    for i in sorted(set(bracket_order.values()) - {0}):
         for eins in sorted([k for k, v in bracket_order.items() if v == i], reverse=True):
-            expr = expression[eins[0]+1:eins[1]]
-            print(expr, '=', eval(expr))
+            expr = expression[eins[0] + 1:eins[1]]
+
             for nuller in sorted(brackets[eins], reverse=True):
-                expr = '{}{}{}'.format(expr[:nuller[0]-eins[0]-1], brackets[nuller],  expr[nuller[1]-eins[0]:])
+                expr = '{}{}{}'.format(expr[:nuller[0] - eins[0] - 1], brackets[nuller], expr[nuller[1] - eins[0]:])
                 brackets.pop(nuller)
                 expr = expr.replace(' ', '').replace('--', '+').replace('+-', '-')
 
-            print(eval(expr))
             brackets[eins] = str(eval_flat_expression(regex.findall(expr)))
-            print(brackets[eins])
 
     for br in sorted(brackets.keys(), reverse=True):
         expression = '{}{}{}'.format(expression[:br[0]], brackets[br], expression[br[1] + 1:])
 
     expression = expression.replace(' ', '').replace('--', '+').replace('+-', '-')
-
     return eval_flat_expression(regex.findall(expression))
-    # eval_flat_expression(regex.findall('-64.0*2.41025641025641--68.0'))
 
 
 def eval_flat_expression(expression):
@@ -82,12 +76,11 @@ def eval_flat_expression(expression):
 
 
 if __name__ == '__main__':
-
     # assert calc('(2+2) + (3-3) * (2 / (2 + 3.33) * 4) - -6 * (3-(3/1))') == 4.0
     assert calc('(-83)/(-60/59*-(20))*(35*-(((-(78+92))))+21)') == -24366.655833333334
     assert calc('(-76)/(-14+98+(19))/(17/-(((-(-84*-65))))-26)') == 0.028382786499353052
     assert calc("3 -(-1)") == 4
-    assert calc('(-64) * (94 / -13 / -(3)) - (62 * -((((-45 + 46)))) + -6)') == -86.25641025641025 # carefull *-
+    assert calc('(-64) * (94 / -13 / -(3)) - (62 * -((((-45 + 46)))) + -6)') == -86.25641025641025  # carefull *-
     calc('13 * 36 / 5 / -76 * 71 - 70 * -21 - -80')
     calc('-7 * -(6 / 3)') == 14
     calc('(2 / (2 + 3.33) * 4) - -6') == 7.50093808630394
@@ -98,6 +91,5 @@ if __name__ == '__main__':
 
     calc('-(-(-(-1)))')
     calc('(((((-1)))))')
-
 
     calc('-(22) * (-14 / 32 + (52)) - (-12 + (((-(23 / 74)))) + 50)')
