73b2963b2845a06b7bc0e760a831005fe7c5f440
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-01-09 18:48:05 +0100

    Node neighbours corrected + first connectivity check attempt

--- Pathfinder1.py
+++ Pathfinder1.py
@@ -5,6 +5,7 @@ class Node:
         self.neighbours = []
 
 
+
 class Graph:
     def __init__(self, map):
         # parsing
@@ -12,15 +13,42 @@ class Graph:
         self.rdim = len(m[0])
         self.cdim = len(m)
 
-        self.nodes = {(i, i // self.rdim): Node((i, i // self.rdim), symb)
+        tupler = lambda i: (i % self.rdim, i // self.rdim)
+        self.nodes = {tupler(i): Node(tupler(i), symb)
                       for i, symb in enumerate(map.replace('\n', ''))}
 
+        for node in [node for node in self.nodes.values() if node.symb != 'W']:
+            node.neighbours = [pos for pos in self._find_neighbours(node.position)
+                               if self.nodes[pos].symb != 'W']
 
-    def connect_path(self, start=(0, 0), end=None):
+    def connect_path(self, start=(0, 0), end=None, path = []):
+        """In a backtracking manner find weather or not their is a path beween start and end"""
         # Finding non cyclic paths: https://www.python.org/doc/essays/graphs/
         if end is None:
-            pass
-        pass
+            end = (self.rdim - 1, self.cdim - 1)
+
+        path = path + [start]
+        if start == end:
+            return True
+        for neighb in self.nodes[start].neighbours:
+            if neighb not in path:
+                newpath = self.connect_path(neighb)
+                if newpath:
+                    return newpath
+            return None
+
+
+        # def find_path(graph, start, end, path=[]):
+        #     path = path + [start]
+        #     if start == end:
+        #         return path
+        #     if not graph.has_key(start):
+        #         return None
+        #     for node in graph[start]:
+        #         if node not in path:
+        #             newpath = find_path(graph, node, end, path)
+        #             if newpath: return newpath
+        #     return None
 
     def _find_neighbours(self, position):
         """returns the set of horizontal an vertical neighbours"""
@@ -31,8 +59,9 @@ class Graph:
                      if cond(r + i, c + j) and cond(r + i, c + j))
         return neighb
 
+
 def path_finder(map):
-    return Graph(map).connect_path(start=(0, 0))
+    return bool(Graph(map).connect_path(start=(0, 0)))
 
 
 if __name__ == '__main__':
