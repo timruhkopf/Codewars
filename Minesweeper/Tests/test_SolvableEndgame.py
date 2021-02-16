afaf372b0e9c2f820e98c28958e1aa3b15b71c8a
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-02-16 10:01:01 +0100

    added and eddited tests for SolvableEndgame & Supersets. Supersets finds a bomb it shouldnt

--- Minesweeper/Tests/test_SolvableEndgame.py
+++ Minesweeper/Tests/test_SolvableEndgame.py
@@ -2,6 +2,7 @@ import unittest
 
 from ..Board.Game import Game
 from ..Board.Solution import Solution
+from ..Strategies import Strategy_Superset, Strategy_Endgame
 from ..Util import board
 
 
@@ -10,13 +11,25 @@ class TestSolvableEndgame(unittest.TestCase):
     def tearDown(self) -> None:
         solution = Solution(board(self.result))
         m = Game(board=solution.covered_board, n=solution.n, context=solution)
-        m.solve()
+
+        while bool(m.zeros):
+            zero = m.zeros.pop()
+            m.open(*zero)
+            placedzeros = set(n.position for n in m.clues.values() if n.clue == 0)
+            m.zeros.difference_update(placedzeros)
+
+        # (1) find sets of nodes, that are jointly informative
+        Strategy_Superset.execute(m)
+
+        Strategy_Endgame.simple(m)
+        Strategy_Endgame.sequential_combinations(m)
 
         self.assertEqual(m.board, board(self.result))
 
         del self.result
 
     def test_endgame(self):
+        # FIXME: after open zeros, the
         self.result = """
           0 1 x x 1
           0 1 3 4 3
@@ -26,6 +39,27 @@ class TestSolvableEndgame(unittest.TestCase):
           0 1 2 x 1
           0 1 x 2 1
           """
+
+        solution = Solution(board(self.result))
+        m = Game(board=solution.covered_board, n=solution.n, context=solution)
+
+        while bool(m.zeros):
+            zero = m.zeros.pop()
+            m.open(*zero)
+            placedzeros = set(n.position for n in m.clues.values() if n.clue == 0)
+            m.zeros.difference_update(placedzeros)
+
+        Strategy_Superset.double(m)  # Fixme: it finds a bomb which it shouldnt
+
+        self.assertEqual(
+            m.board, board("""
+            0 1 x ? ?
+            0 1 3 ? ?
+            0 0 1 ? ?
+            0 0 1 ? ?
+            0 0 1 ? ?
+            0 1 2 ? ?
+            0 1 x ? ?"""))
 
     def test_endgame_simple(self):
         """all remaining ? after communication must be bombs due to count of bombs
@@ -59,31 +93,49 @@ class TestSolvableEndgame(unittest.TestCase):
         0 0 0 0 0 0 0 0 0 0 0
         """
 
-    # def test_endgameStrategy2(self):
-    #       """This test has three deterministic shapes solvable with communication
-    #       only & one pattern on the right, that is not deterministically solvable"""
-    # TODO check why this test's communication strategy fails
+    def test_endgameStrategy2(self):
+        """This test has three deterministic shapes solvable with communication
+          only & one pattern on the right, that is not deterministically solvable"""
 
-    #     self.result = """
-    #     1 x 1 0 0 2 x 2 1 x 1 0 0 1 x x 1
-    #     1 1 1 0 0 2 x 3 2 1 1 0 0 1 3 4 3
-    #     0 0 0 0 0 1 2 x 1 0 0 0 0 0 1 x x
-    #     0 0 0 0 0 0 1 1 1 0 0 0 0 0 1 2 2
-    #     0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1
-    #     1 1 1 0 0 0 0 0 0 0 0 0 0 1 2 x 1
-    #     1 x 1 0 0 0 0 0 0 0 0 0 0 1 x 2 1
-    #     """
+        self.result = """
+        1 x 1 0 0 2 x 2 1 x 1 0 0 1 x x 1
+        1 1 1 0 0 2 x 3 2 1 1 0 0 1 3 4 3
+        0 0 0 0 0 1 2 x 1 0 0 0 0 0 1 x x
+        0 0 0 0 0 0 1 1 1 0 0 0 0 0 1 2 2
+        0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1
+        1 1 1 0 0 0 0 0 0 0 0 0 0 1 2 x 1
+        1 x 1 0 0 0 0 0 0 0 0 0 0 1 x 2 1
+        """
 
-    # def test_endgame4(self):
-    #     """test endgame for more than 3 remaining bombs"""
-    #     self.result = """
-    #     0 0 0 1 x 1 1 x 1 0 0 0 0 0 1 1 1 0 0 1 x 3 x 3 1 2 1
-    #     1 1 0 1 1 1 1 1 1 0 0 0 0 0 1 x 1 1 1 2 1 3 x 3 x 2 x
-    #     x 2 1 1 0 0 0 0 0 0 1 1 1 0 1 1 1 1 x 1 0 2 2 3 1 3 2
-    #     1 2 x 1 0 0 0 0 0 0 1 x 1 0 0 0 0 1 1 1 0 1 x 2 1 2 x
-    #     0 1 1 1 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 1 2 3 x 2 1
-    #     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 x 2 1 0
-    #     """
+    def test_endgame4_partial(self):
+        self.result = """
+        0 2 x 3 1 2 1
+        0 2 x 3 x 2 x
+        0 2 2 3 1 3 2
+        0 1 x 2 1 2 x
+        0 1 2 3 x 2 1
+        0 0 1 x 2 1 0
+        """
+        # after open zeros
+        """
+        0 2 x ? ? ? ?
+        0 2 x 3 ? ? ? 
+        0 2 2 3 ? ? ?
+        0 1 x 2 ? ? ?
+        0 1 2 ? ? 2 1
+        0 0 1 ? ? 1 0
+        """
+
+    def test_endgame4(self):
+        """test endgame for more than 3 remaining bombs"""
+        self.result = """
+        0 0 0 1 x 1 1 x 1 0 0 0 0 0 1 1 1 0 0 1 x 3 x 3 1 2 1
+        1 1 0 1 1 1 1 1 1 0 0 0 0 0 1 x 1 1 1 2 1 3 x 3 x 2 x
+        x 2 1 1 0 0 0 0 0 0 1 1 1 0 1 1 1 1 x 1 0 2 2 3 1 3 2
+        1 2 x 1 0 0 0 0 0 0 1 x 1 0 0 0 0 1 1 1 0 1 x 2 1 2 x
+        0 1 1 1 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 1 2 3 x 2 1
+        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 x 2 1 0
+        """
     # TODO check if it works, when the n
     #  if bool(self.remain_bomb) and self.remain_bomb <= 3:
     #  self.remain_bomb <= 3 condition is changed to more bombs!
@@ -96,4 +148,4 @@ class TestSolvableEndgame(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(exit=False)
--- Minesweeper/Tests/test_Supersets.py
+++ Minesweeper/Tests/test_Supersets.py
@@ -1,9 +1,39 @@
 import unittest
 
+from ..Board.Game import Game
+from ..Board.Solution import Solution
+from ..Strategies import Strategy_Superset
+from ..Util import board
+
+
+def prep_test(self):
+    """tearDown like method, that sets up the basic test enviroment of the board"""
+    solution = Solution(board(self.result))
+    m = Game(board=solution.covered_board, n=solution.n, context=solution)
+
+    # opening a single zero suffices to recursively communicate ('open') all zeros
+    # & remove them from m.zeros entirely
+    while bool(m.zeros):
+        zero = m.zeros.pop()
+        m.open(*zero)
+
+        placedzeros = set(n.position for n in m.clues.values() if n.clue == 0)
+        m.zeros.difference_update(placedzeros)
+
+    return solution, m
+
 
 class TestSupersets(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_121pattern(self):
+        self.result = """
+        0 0 0
+        1 2 1
+        x 2 x
+        """
+
+        solution, m = prep_test(self)
+        Strategy_Superset.tripple(m)
 
 
 if __name__ == '__main__':
