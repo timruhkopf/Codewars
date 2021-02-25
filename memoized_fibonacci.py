545e67c6c47c114184d1713ceff4f6ab40942956
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-02-25 09:43:00 +0100

    restructured scripts into 'Training' folders; by difficulty & class relation

--- memoized_fibonacci.py
+++ memoized_fibonacci.py
@@ -1,4 +1,3 @@
-
 def memoize(func):
     def wrapper(n):
         if n in mem.keys():
@@ -10,22 +9,27 @@ def memoize(func):
     mem = {}
     return wrapper
 
+
 @memoize
 def fibonacci(n):
     if n in [0, 1]:
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 
+
+# (unittests) ------------------------------------------------------------------
+import unittest
+
+
+class MemFib(unittest.TestCase):
+    def test_small_numbers(self):
+        self.assertEqual(fibonacci(1), 1)
+
+    def test_large_numbers(self):
+        self.assertEqual(fibonacci(70), 190392490709135)
+        self.assertEqual(fibonacci(60), 1548008755920)
+        self.assertEqual(fibonacci(50), 12586269025)
+
+
 if __name__ == '__main__':
-
-
-    import unittest
-
-
-    class MemFib(unittest.TestCase):
-        def test_large_numbers(self):
-            self.assertEqual(fibonacci(70), 190392490709135)
-            self.assertEqual(fibonacci(60), 1548008755920)
-            self.assertEqual(fibonacci(50), 12586269025)
-
-    unittest.main()
+    unittest.main(exit=False)
