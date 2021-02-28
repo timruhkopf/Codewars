41be8dc94373fa0091c9b7aa6d14435c11fdfb19
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-02-28 11:11:57 +0100

    created Strategyforwardbackward_sorted.py: a variant (inefficent as it turns out) of Strategyforwardbackward.py

---
+++ Sudoku/Strategies/Strategyforwardbackward_sorted.py
@@ -0,0 +1,15 @@
+from .Strategyforwardbackward import Strategyforwardbackward, deque, Experiment
+
+
+class Strategyforwardbackward_sorted(Strategyforwardbackward):
+    def execute_sorted_zeros(sudoku):
+        """DEPREC: try to do good decisions early-on: the number of calls to forward is
+                by far greater (10x +/-) than in the linear execution. -- this strategy reverts far later,
+                as the density of information gain for the next position is smaller """
+        experiment = Experiment(sudoku.problem, sudoku.zeros)
+        experiment.unvisited = deque(sorted(sudoku.zeros, key=lambda zero: len(experiment.options(*zero))))
+
+        r, c = experiment.nextzero()
+        options = experiment.options(r, c)
+        Strategyforwardbackward_sorted.forward(experiment, r, c, options)
+        print('no.calls to forward:', Strategyforwardbackward_sorted.forward.calls)  # FIXME: this seems a bit high.
