79f01eef117a21279fdcf369142ecb0380e72973
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-03-01 09:15:07 +0100

    TESTS: added multiple solutions example

--- Sudoku/Test/Test_boards/test_multiple_solutions.py
+++ Sudoku/Test/Test_boards/test_multiple_solutions.py
@@ -7,8 +7,46 @@ class MyTestCase(unittest.TestCase):
     """by kata's conventions, multiple solutions raise 'invalid grid' Valueerror"""
 
     def tearDown(self) -> None:
+        s = Sudoku(self.problem)
         with self.assertRaises(ValueError):
-            Sudoku(self.problem)
+            s.solve()
+        self.assertTrue(len(s.solutions) > 1)
+        for sol in s.solutons:
+            self.assertTrue(sol in self.solutions)
+
+    def test_square_switch(self):
+        """the two solutions are almost identical; except a switch in row 5 & 6"""
+        self.problem = [[9, 0, 6, 0, 7, 0, 4, 0, 3],
+                        [0, 0, 0, 4, 0, 0, 2, 0, 0],
+                        [0, 7, 0, 0, 2, 3, 0, 1, 0],
+                        [5, 0, 0, 0, 0, 0, 1, 0, 0],
+                        [0, 4, 0, 2, 0, 8, 0, 6, 0],
+                        [0, 0, 3, 0, 0, 0, 0, 0, 5],
+                        [0, 3, 0, 7, 0, 0, 0, 5, 0],
+                        [0, 0, 7, 0, 0, 5, 0, 0, 0],
+                        [4, 0, 5, 0, 1, 0, 7, 0, 8]]
+
+        self.solutions = [
+            [[9, 2, 6, 5, 7, 1, 4, 8, 3],
+             [3, 5, 1, 4, 8, 6, 2, 7, 9],
+             [8, 7, 4, 9, 2, 3, 5, 1, 6],
+             [5, 8, 2, 3, 6, 7, 1, 9, 4],
+             [1, 4, 9, 2, 5, 8, 3, 6, 7],
+             [7, 6, 3, 1, 9, 4, 8, 2, 5],  # 9, 4
+             [2, 3, 8, 7, 4, 9, 6, 5, 1],  # 4, 9
+             [6, 1, 7, 8, 3, 5, 9, 4, 2],
+             [4, 9, 5, 6, 1, 2, 7, 3, 8]],
+
+            [[9, 2, 6, 5, 7, 1, 4, 8, 3],
+             [3, 5, 1, 4, 8, 6, 2, 7, 9],
+             [8, 7, 4, 9, 2, 3, 5, 1, 6],
+             [5, 8, 2, 3, 6, 7, 1, 9, 4],
+             [1, 4, 9, 2, 5, 8, 3, 6, 7],
+             [7, 6, 3, 1, 4, 9, 8, 2, 5],  # 4, 9
+             [2, 3, 8, 7, 9, 4, 6, 5, 1],  # 9, 4
+             [6, 1, 7, 8, 3, 5, 9, 4, 2],
+             [4, 9, 5, 6, 1, 2, 7, 3, 8]]
+        ]
 
 
 if __name__ == '__main__':
